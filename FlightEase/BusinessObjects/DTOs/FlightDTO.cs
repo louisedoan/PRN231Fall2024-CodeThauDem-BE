59f812fd720cb6934a2f12e2b299d9bfe7ea8809
phamduchuy <huypd284@gmail.com>
2024-10-01 14:44:17 +0700

    Fix : flight DTO

--- FlightEase/BusinessObjects/DTOs/FlightDTO.cs
+++ FlightEase/BusinessObjects/DTOs/FlightDTO.cs
@@ -8,15 +8,14 @@
 
         public int? FlightNumber { get; set; }
 
-        public int? DepartureLocation { get; set; } // Lưu ID của địa điểm khởi hành
-
-        public string? DepartureLocationName { get; set; } // Lưu tên địa điểm khởi hành
+        public int? DepartureLocation { get; set; } 
+        public string? DepartureLocationName { get; set; } 
 
         public DateTime? DepartureTime { get; set; }
 
-        public int? ArrivalLocation { get; set; } // Lưu ID của địa điểm đến
+        public int? ArrivalLocation { get; set; } 
 
-        public string? ArrivalLocationName { get; set; } // Lưu tên địa điểm đến
+        public string? ArrivalLocationName { get; set; } 
 
         public DateTime? ArrivalTime { get; set; }
 
