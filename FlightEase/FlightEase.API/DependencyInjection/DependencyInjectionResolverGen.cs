bdbdfdebb953aac3419d34cf472c4a7e82eba465
VanMinh15 <minhcangthang@gmail.com>
2024-10-04 16:28:16 +0700

    update vnpay sandbox and db script

--- FlightEase/FlightEase.API/DependencyInjection/DependencyInjectionResolverGen.cs
+++ FlightEase/FlightEase.API/DependencyInjection/DependencyInjectionResolverGen.cs
@@ -2,6 +2,7 @@ using FlightEase.Services.Services;
 using FlightEaseDB.BusinessLogic.Services;
 using FlightEaseDB.Services.Services;
 using Repositories.Repositories;
+using Services.VnPay;
 
 namespace FlightEaseDB.BusinessLogic.Generations.DependencyInjection
 {
@@ -12,37 +13,39 @@ namespace FlightEaseDB.BusinessLogic.Generations.DependencyInjection
 
             services.AddScoped<IFlightService, FlightService>();
             services.AddScoped<IFlightRepository, FlightRepository>();
-        
+
             services.AddScoped<IFlightRouteService, FlightRouteService>();
             services.AddScoped<IFlightRouteRepository, FlightRouteRepository>();
-        
+
             services.AddScoped<IMembershipService, MembershipService>();
             services.AddScoped<IMembershipRepository, MembershipRepository>();
-        
+
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IOrderRepository, OrderRepository>();
-        
+
             services.AddScoped<IOrderDetailService, OrderDetailService>();
             services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
-        
+
             services.AddScoped<IPaymentService, PaymentService>();
             services.AddScoped<IPaymentRepository, PaymentRepository>();
-        
+
             services.AddScoped<IPilotService, PilotService>();
             services.AddScoped<IPilotRepository, PilotRepository>();
-        
+
             services.AddScoped<IPlaneService, PlaneService>();
             services.AddScoped<IPlaneRepository, PlaneRepository>();
-        
-       
+
+
             services.AddScoped<ISeatService, SeatService>();
             services.AddScoped<ISeatRepository, SeatRepository>();
-        
+
             services.AddScoped<ISeatFlightService, SeatFlightService>();
             services.AddScoped<ISeatFlightRepository, SeatFlightRepository>();
-        
+
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<IVnPayService, VnPayService>();
         }
     }
 }
---
+++ FlightEase/Services/VnPay/VnPayLibrary.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Services.VnPay
+{
+    public class VnPayLibrary
+    {
+        public const string VERSION = "2.1.0";
+        private SortedList<string, string> _requestData = new SortedList<string, string>(new VnPayCompare());
+        private SortedList<string, string> _responseData = new SortedList<string, string>(new VnPayCompare());
+
+        public void AddRequestData(string key, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                _requestData.Add(key, value);
+            }
+        }
+
+        public void AddResponseData(string key, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                _responseData.Add(key, value);
+            }
+        }
+
+        public string GetResponseData(string key)
+        {
+            return _responseData.TryGetValue(key, out var retValue) ? retValue : string.Empty;
+        }
+
+        #region Request
+
+        public string CreateRequestUrl(string baseUrl, string vnp_HashSecret)
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _requestData)
+            {
+                if (!string.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+
+            string queryString = data.ToString().TrimEnd('&');
+            string vnp_SecureHash = Utils.HmacSHA512(vnp_HashSecret, queryString);
+            string paymentUrl = $"{baseUrl}?{queryString}&vnp_SecureHash={vnp_SecureHash}";
+
+            return paymentUrl;
+        }
+
+        #endregion
+
+        #region Response process
+
+        public bool ValidateSignature(string inputHash, string secretKey)
+        {
+            string rspRaw = GetResponseDataString();
+            string myChecksum = Utils.HmacSHA512(secretKey, rspRaw);
+            return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private string GetResponseDataString()
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _responseData)
+            {
+                if (!string.IsNullOrEmpty(kv.Value) && kv.Key != "vnp_SecureHashType" && kv.Key != "vnp_SecureHash")
+                {
+                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1); // remove last '&'
+            }
+            return data.ToString();
+        }
+
+        #endregion
+    }
+
+    public class Utils
+    {
+        public static string HmacSHA512(string key, string inputData)
+        {
+            var hash = new StringBuilder();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+            using (var hmac = new HMACSHA512(keyBytes))
+            {
+                byte[] hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+
+        public static string GetIpAddress(HttpContext context)
+        {
+            var ipAddress = string.Empty;
+            try
+            {
+                var remoteIpAddress = context.Connection.RemoteIpAddress;
+
+                if (remoteIpAddress != null)
+                {
+                    if (remoteIpAddress.AddressFamily == AddressFamily.InterNetworkV6)
+                    {
+                        remoteIpAddress = Dns.GetHostEntry(remoteIpAddress).AddressList
+                            .FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+                    }
+
+                    if (remoteIpAddress != null) ipAddress = remoteIpAddress.ToString();
+
+                    return ipAddress;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Invalid IP:" + ex.Message;
+            }
+
+            return "127.0.0.1";
+        }
+    }
+
+    public class VnPayCompare : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == y) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+            var vnpCompare = CompareInfo.GetCompareInfo("en-US");
+            return vnpCompare.Compare(x, y, CompareOptions.Ordinal);
+        }
+    }
+}
