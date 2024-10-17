eee20328db5a67da4e053ba59d1678b05e92b38d
tuan-anh-83 <tuananh83cbt@gmail.com>
2024-10-17 16:19:09 +0700

    CRUD Order API

--- FlightEase/BusinessObjects/DTOs/OrderDTO.cs
+++ FlightEase/BusinessObjects/DTOs/OrderDTO.cs
@@ -2,5 +2,13 @@
 {
     public class OrderDTO
     {
+        public int OrderId { get; set; }
+        public int? UserId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? TripType { get; set; }
+        public string? Status { get; set; }
+        public double? TotalPrice { get; set; }
+        public string? DepartureLocation { get; set; } // Lấy từ Flight
+        public string? ArrivalLocation { get; set; }   // Lấy từ Flight
     }
 }
